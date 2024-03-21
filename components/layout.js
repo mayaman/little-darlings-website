449f8ac8f97ea4a5567cb1ac6bbac8689dea687d
Maya Man <mayaman26@gmail.com>
2024-03-21 14:04:43 -0400

    adding fonts

--- components/layout.js
+++ components/layout.js
@@ -15,6 +15,8 @@ export default function Layout({ children, home }) {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Bowlby+One&family=Jockey+One&display=swap" rel="stylesheet"></link>
+                <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Share+Tech&display=swap" rel="stylesheet"></link>
+                {/* <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet"></link> */}
                 {/* <link href="https://fonts.googleapis.com/css2?family=Bayon&family=Dancing+Script:wght@600&family=IM+Fell+French+Canon+SC&family=Irish+Grover&family=Oswald:wght@400;500&family=Paytone+One&family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,900&family=Rum+Raisin&family=Yuji+Mai&family=Yusei+Magic&display=swap" rel="stylesheet" /> */}
                 <meta
                     name="description"
